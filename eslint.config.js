4c0cf8af370962e8eefda3a2855113629c4ffbb5
Thodoris Greasidis <thgreasi@gmail.com>
2024-12-17 22:43:07 +0200

    Replace gulp, coffeelint & mochainon with tsc, @balena/lint, mocha, chai & sinon
    Change-type: patch

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+});
+module.exports = [
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
+	...require('@balena/lint/config/eslint.config'),
+	...compat.config({
+		parserOptions: {
+			project: './tsconfig.dev.json',
+			sourceType: 'module',
+		},
+	}),
+];
